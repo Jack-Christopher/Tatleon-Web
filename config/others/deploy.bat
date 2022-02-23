d2f3ba6dcc3f9188778ed082e0bb67ab35a5afe2
Jack Christopher <jhuaihuah@unsa.edu.pe>
2022-02-23 14:44:50 -0500

    Minor changes

--- config/others/deploy.bat
+++ config/others/deploy.bat
@@ -4,7 +4,21 @@
 @REM for %%f in (*.*) do @ ncftpput -R -v -u "epiz_29440885" -p "YD0FdeMPKhs" ftpupload.net /htdocs %%f
 @REM for /d %%f in (*.*) do @ ncftpput -R -v -u "epiz_29440885" -p "YD0FdeMPKhs" ftpupload.net /htdocs %%f
 
-cd ../
+@REM cd ../../
 
-for /F "tokens=*" %%f in (folders.txt) do @ ncftpput -R -v -u "epiz_29440885" -p "YD0FdeMPKhs" ftpupload.net /htdocs/laravel %%f
-for /F "tokens=*" %%f in (files.txt) do @ ncftpput -R -v -u "epiz_29440885" -p "YD0FdeMPKhs" ftpupload.net /htdocs/laravel %%f
+@REM for /F "tokens=*" %%f in (folders.txt) do @ ncftpput -R -v -u "epiz_29440885" -p "YD0FdeMPKhs" ftpupload.net /htdocs/laravel %%f
+@REM for /F "tokens=*" %%f in (files.txt) do @ ncftpput -R -v -u "epiz_29440885" -p "YD0FdeMPKhs" ftpupload.net /htdocs/laravel %%f
+
+
+
+
+@REM cd ../../
+@REM cd public
+
+@REM for %%f in (*.*) do @ ncftpput -R -v -u "ezyro_30877474" -p "ss35bfaurms" ftpupload.net /htdocs %%f
+@REM for /d %%f in (*.*) do @ ncftpput -R -v -u "ezyro_30877474" -p "ss35bfaurms" ftpupload.net /htdocs %%f
+
+@REM @REM cd ../../
+
+@REM for /F "tokens=*" %%f in (folders.txt) do @ ncftpput -R -v -u "ezyro_30877474" -p "ss35bfaurms" ftpupload.net /htdocs/laravel %%f
+@REM for /F "tokens=*" %%f in (files.txt) do @ ncftpput -R -v -u "ezyro_30877474" -p "ss35bfaurms" ftpupload.net /htdocs/laravel %%f
--- config/others/post_update.bat
+++ config/others/post_update.bat
@@ -1,5 +1,5 @@
 @REM Bootstrap
-cd ../
+cd ../../
 if exist "public/bootstrap" rmdir /S/Q "public/bootstrap"
 mkdir "public/bootstrap"
 Xcopy "vendor/twbs/bootstrap/dist" "public/bootstrap" /E/H/C/I
