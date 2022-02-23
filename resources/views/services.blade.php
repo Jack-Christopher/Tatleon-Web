61ee77f89d0679fc1618fd315368aa2c8df884c4
Jack Christopher <jhuaihuah@unsa.edu.pe>
2022-02-23 14:49:24 -0500

    Services policies updated

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -14,90 +14,133 @@
 
 <div class="container">
     <h1 class="display-3" align="center">Servicios</h1>
-    <h5 align="center">
-        [TODO: Update with current policies]
-    </h5>
     <br>
 
     <div class="alert alert-success" role="alert">
-      <h2> Repositorio de Enlaces</h2>
-      <h5> Propósito: </h5>
-      <p> Almacenar y poner a disposición enlaces de trabajos realizados por los estudiantes
+    <h2> Repositorio de Enlaces</h2>
+    <h5> Propósito: </h5>
+    <p> Almacenar y poner a disposición enlaces de trabajos realizados por los estudiantes
         de la Universidad Nacional de San Agustín que han sido subidos a otras plataformas con el propósito de
         difundirlo entre los estudiantes de la UNSA y puedan tener una mayor cantidad de reacciones.</p>
-      <h5> Funcionamiento: </h5>
-      <p>
+    <h5> Funcionamiento: </h5>
+    <p>
         Solo las personas que se hayan registrado en Tatleon Web podrán ingresar enlaces relativos a su escuela,
         los cuales serán almacenados en la plataforma.
-        Cualquier persona, no es necesario que se haya registrado, podrá ver los enlaces almacenados en su totalidad.
+        <br>
+        Cualquier persona (sin necesidad de registrarse), podrá ver los enlaces almacenados en su totalidad.
         <br><br>
         No podrán subir enlaces a la Vista de una escuela:
-      <ul>
+    <ul>
         <li> Los que no se hayan registrado </li>
         <li> Los que no pertenezcan a esa escuela </li>
-      </ul>
-      </p>
+    </ul>
+    </p>
     </div>
 
     <div class="alert alert-warning" role="alert">
-      <h2> Registro de Docentes</h2>
-      <h5> Propósito: </h5>
-      <p> Almacenar y poner a disposición el "Registro" de los docentes, ésto es, una serie de datos importantes sobre 
-          los docentes como su preparación profesional y además un conjunto de comentarios subidos por
-        los delegados de los cursos que han sido dictados por estos docentes de la Universidad Nacional de San Agustín.
+    <h2> Registro de Docentes</h2>
+    <h5> Propósito: </h5>
+    <p> Almacenar y poner a disposición el "Registro" de los docentes, ésto es, una serie de datos importantes sobre 
+        los docentes como su preparación profesional y además un conjunto de comentarios subidos por
+        los estudiantes que hayan llevado los cursos dictados por estos docentes de la Universidad Nacional de San Agustín.
         </p>
-      <h5> Funcionamiento: </h5>
-      <p>
+    <h5> Funcionamiento: </h5>
+    <p>
         Los docentes podrán subir los datos que quisieran hacer público de sí mismos.
-        Solo los usuarios registrados como delegados o los docentes podrán agregar un docente a la plataforma
-        y además podrán ingresar un comentario sobre los docentes que ya se encuentren en la plataforma.
-        Antes de agregar a un docente se debe verificar si ya se encuentra en la plataforma.
-        Antes de agregar un comentario el/la delegado(a) debe hacer un consenso con los estudiantes para tener comentarios adecuados y justos.
+        Solo los usuarios registrados como estudiantes podrán ingresar un comentario sobre los docentes que ya 
+        se encuentren en la plataforma.
+        <br>
+        <b> Se deben cumplir las siguientes reglas en la publicación de comentarios: </b>
+        <ul>
+            <li> No se permiten comentarios que no se fundamenten en la realidad. </li>
+            <li> No se permiten comentarios que atenten contra la moral, la ética o la dignidad de los docentes. </li>
+            <li> No se permiten comentarios que tengan contenido sexual, racista, violento, amenazante, degradante o datos personales. </li>
+        </ul>
+        
         El usuario podrá ver todos los comentarios almacenados en la plataforma.
         <br><br>
         No podrán subir comentarios sobre un docente:
-      <ul>
+    <ul>
         <li> Los que no se hayan registrado </li>
-        <li> Los que no sean delegados </li>
-      </ul>
-      </p>
+    </ul>
+    </p>
     </div>
 
     <div class="alert alert-danger" role="alert">
-      <h2> Recursos Compartidos</h2>
-      <h5> Propósito: </h5>
-      <p> Almacenar y poner a disposición Recursos Académicos subidos por los estudiantes
+    <h2> Recursos Compartidos</h2>
+    <h5> Propósito: </h5>
+    <p> Almacenar y poner a disposición Recursos Académicos subidos por los estudiantes
         de la Universidad Nacional de San Agustín.
         Con el propósito de que los estudiantes que están en grados inferiores o en otras escuelas/facultades puedan 
         estudiar y/o practicar con anticipación y tener una educación más integral complementando con estos recursos
         los cursos que estén llevando.</p>
-      </p>
-      <h5> Funcionamiento: </h5>
-      <p>
-        La idea es que el /la delegado(a) del curso cree una carpeta pública en Google Drive con el nombre del curso y dentro de esta carpeta
-        los estudiantes suban los recursos que se usen en el desarrollo del curso.
+    </p>
+    <h5> Funcionamiento: </h5>
+    <p>
+        La idea es que cualquier estudiante que esté llevando el curso cree una carpeta en Google Drive dentro de la cual
+        suba recursos que considere útiles para el curso.
+        Lo que debe subir a esta plataforma es el enlace de la carpeta en Google Drive.
         Cualquier usuario podrá ver todos los recursos almacenados en la plataforma.
-        <br><br>
+        <br>
+        <b> Se deben cumplir las siguientes reglas en la publicación de recursos: </b>
+        <ul>
+            <li> No se permiten recursos que infringan derechos de autor. </li>
+            <li> No se permiten recursos que atenten contra la moral, la ética o la dignidad de los estudiantes. </li>
+            <li> No se permiten recursos que tengan contenido sexual, racista, violento, amenazante, degradante o que contengan datos personales. </li>
+        </ul>
+
+        <br>
         No podrán subir Recursos de un curso:
-      <ul>
+    <ul>
         <li> Los que no se hayan registrado </li>
         <li> Los que no sean delegados </li>
-      </ul>
-      </p>
+    </ul>
+    </p>
+    </div>
+
+
+    <div class="alert alert-secondary" role="alert">
+    <h2> Ayuda Externa</h2>
+    <h5> Propósito: </h5>
+    <p> Almacenar y poner a disposición peticiones de ayuda académica de los estudiantes de la Universidad Nacional de San Agustín.
+        Con el propósito de que algún estudiante que pueda ayudarlo academicamente se ponga en contacto con los estudiantes.
+        </p>
+    </p>
+    <h5> Funcionamiento: </h5>
+    <p>
+        <h6> ¿Qué se entiende por Ayuda Académica? </h6>
+        <p>
+            Ayuda académica se entiende como la ayuda que un estudiante puede brindar a otro estudiante de la misma escuela o 
+            de otra escuela para la realización de trabajos o proyectos, lecciones, orientación o en otras actividades 
+            que pueden ser remuneradas previo acuerdo de los estudiantes.
+        </p>
+        La idea es que cualquier estudiante pueda solicitar ayuda académica publicamente para que algún estudiante que tenga la 
+        posibilidad de ayudarlo pueda hacerlo.
+        Cualquier usuario podrá ver todos las peticiones de ayuda registradas en la plataforma.
+        <br>
+        <b> Se deben cumplir las siguientes reglas en la publicación de peticiones de ayuda: </b>
+        <ul>
+            <li> No se permiten peticiones de ayuda que sean falsos o de broma. </li>
+            <li> No se permiten peticiones de ayuda que atenten contra la moral, la ética o la dignidad de los estudiantes. </li>
+            <li> No se permiten peticiones de ayuda que tengan contenido sexual, racista, violento, amenazante, degradante o que contengan datos personales. </li>
+        </ul>
+
+        <br>
+        No podrán pedir Ayuda Académica:
+    <ul>
+        <li> Los que no se hayan registrado </li>
+    </ul>
+    </p>
     </div>
 
     <div class="alert alert-info" role="alert">
-      <h2> Coming soon...</h2>
-      <p> Pronto estaremos implementando nuevos servicios.</p>
-      <br>
-      <p> Puedes dejar tus sugerencias en el siguiente formulario.</p>
-      <a href="https://forms.gle/ypVaTyf5dG8BZHtL6" target="_blank" class="btn btn-info">Ir a Formulario</a>
+    <h2> Coming soon...</h2>
+    <p> Pronto estaremos implementando nuevos servicios.</p>
+    <br>
+    <p> Puedes dejar tus sugerencias en el siguiente formulario.</p>
+    <a href="https://forms.gle/ypVaTyf5dG8BZHtL6" target="_blank" class="btn btn-info">Ir a Formulario</a>
     </div>
     <br><br>
-  </div>
-</body>
-
-</html>
-
+</div>
 
 @endsection
